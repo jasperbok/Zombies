ccadade8d6a7bfc5d64d1342b07d836a30bc173e
Tim Commandeur <timcommandeur@timcommandeurPC.(none)>
2012-04-18 17:41:30 +0200

    Forgot to add the new classes for the components.

---
+++ src/nl/jasperbok/zombies/entity/component/LifeComponent.java
@@ -0,0 +1,42 @@
+package nl.jasperbok.zombies.entity.component;
+
+import nl.jasperbok.zombies.entity.Entity;
+
+import org.newdawn.slick.Input;
+
+public class LifeComponent extends Component {
+	protected int hp = 1;
+	
+	/**
+	 * 
+	 * @param owner
+	 */
+	public LifeComponent(Entity owner) {
+		this.owner = owner;
+	}
+	
+	/**
+	 * 
+	 * @param owner
+	 * @param hp
+	 */
+	public LifeComponent(Entity owner, int hp) {
+		this(owner);
+		this.hp = hp;
+	}
+	
+	public void update(Input input, int delta) {
+		if (hp == 0) {
+			owner.die();
+		}
+	}
+	
+	public int takeDamage(int damage) {
+		if (damage > hp) {
+			hp = 0;
+		} else {
+			hp -= damage;
+		}
+		return hp;
+	}
+}
---
+++ src/nl/jasperbok/zombies/entity/object/projectile/Bullet.java
@@ -0,0 +1,50 @@
+package nl.jasperbok.zombies.entity.object.projectile;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.Input;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.geom.Vector2f;
+
+import nl.jasperbok.zombies.entity.Entity;
+import nl.jasperbok.zombies.entity.component.DamagingAuraComponent;
+import nl.jasperbok.zombies.entity.component.LifeComponent;
+import nl.jasperbok.zombies.level.Level;
+
+public class Bullet extends Entity {
+	private Image image;
+	
+	/**
+	 * 
+	 * @param level
+	 * @param position
+	 * @param velocity
+	 * @throws SlickException
+	 */
+	public Bullet(Level level, Vector2f position, Vector2f velocity) throws SlickException {
+		this(level, position, velocity, 1);
+	}
+	
+	/**
+	 * 
+	 * @param level
+	 * @param position
+	 * @param velocity
+	 * @param damage
+	 * @throws SlickException
+	 */
+	public Bullet(Level level, Vector2f position, Vector2f velocity, int damage) throws SlickException {
+		this.position = position;
+		this.velocity = velocity;
+		
+		this.image = new Image("data/sprites/entity/object/projectile/bullet.png");
+		
+		addComponent(new LifeComponent(this));
+		addComponent(new DamagingAuraComponent(this, damage));
+	}
+	
+	public void render(GameContainer container, Graphics g) throws SlickException {
+		image.draw(renderPosition.x, renderPosition.y);
+	}
+}
