2fb82242b3c5af8102fb3064e316777719aedb03
Tim Commandeur <timcommandeur@timcommandeurPC.(none)>
2012-04-25 15:31:00 +0200

    Added the breakable floor.

---
+++ src/nl/jasperbok/zombies/entity/building/BreakableFloor.java
@@ -0,0 +1,38 @@
+package nl.jasperbok.zombies.entity.building;
+
+import nl.jasperbok.zombies.entity.Entity;
+import nl.jasperbok.zombies.entity.Player;
+import nl.jasperbok.zombies.entity.component.Component;
+import nl.jasperbok.zombies.entity.component.LifeComponent;
+import nl.jasperbok.zombies.level.Level;
+
+import org.newdawn.slick.Animation;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.Input;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.geom.Rectangle;
+
+public class BreakableFloor extends Entity {
+	public BreakableFloor(Level level) throws SlickException {
+		super.init(level);
+		Animation breakableFloor = new Animation();
+		breakableFloor.addFrame(new Image("data/sprites/entity/building/breakablefloor.png"), 5000);
+		this.anims.put("breakableFloor", breakableFloor);
+		this.currentAnim = this.anims.get("breakableFloor");
+		this.boundingBox = new Rectangle(position.x, position.y, this.currentAnim.getWidth(), this.currentAnim.getHeight());
+		this.isBlocking = true;
+		
+		this.components.add(new LifeComponent(this, 1));
+	}
+	
+	public void update(Input input, int delta) {
+		super.update(input, delta);
+		boundingBox.setBounds(position.x, position.y, this.currentAnim.getWidth(), this.currentAnim.getHeight());
+		
+		Player player = level.env.getPlayer();
+		if (player.position.x <  boundingBox.getMaxX() && player.position.x > boundingBox.getMinX() && Math.abs(boundingBox.getMinY() - player.boundingBox.getMaxY()) < 6) {
+			// break the platform.
+			((LifeComponent)getComponent(Component.LIFE)).takeDamage(1);
+		}
+	}
+}
